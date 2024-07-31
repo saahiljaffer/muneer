6433c0a3d1b98b1618681cbdf4f25a713030db0b
Saahil Jaffer <contact@saahiljaffer.com>
2024-07-31 08:30:31 -0400

    remove timezone

--- src/app/api/gToH/[date]/route.ts
+++ src/app/api/gToH/[date]/route.ts
@@ -1,6 +1,4 @@
 import dayjs from 'dayjs'
-import timezone from 'dayjs/plugin/timezone'
-import utc from 'dayjs/plugin/utc'
 import hijri from '@/lib/hijri'
 import 'dayjs/locale/ar'
 import preParsePostFormat from 'dayjs/plugin/preParsePostFormat'
@@ -9,12 +7,10 @@ export async function GET(
   request: Request,
   { params }: { params: { date: string } },
 ) {
-  dayjs.extend(utc)
-  dayjs.extend(timezone)
   dayjs.extend(hijri)
   dayjs.extend(preParsePostFormat)
 
-  const date = dayjs(params.date).tz('America/Toronto').locale('ar')
+  const date = dayjs(params.date).locale('ar')
 
   return Response.json({
     year: date.format('iYYYY'),
